469bcfe2a4395cd89928e1967629ebe21586c2f8
efimov_a <a.efimov@bpcbt.com>
2021-11-12 01:30:01 +0300

    labguide 5(main2)

--- employees/service.js
+++ employees/service.js
@@ -1,3 +1,9 @@
+import { DATA } from './employees-json';
+
+const employeeMap = {};
+
+export function getEmployees() { return DATA.employees }
+
 function findByName(name, surname) {
     let res = [];
     for (var e of DATA.employees) {
@@ -9,8 +15,7 @@ function findByName(name, surname) {
     return res;
 }
 
-
-function addEmployee(name, surname) {
+export function addEmployee(name, surname) {
     if (!name || name.length==0 || !surname || surname.length==0) {
      throw new Error("name and surname should be not empty");
     }
@@ -20,22 +25,19 @@ function addEmployee(name, surname) {
     }
     let id = max+1;
     DATA.employees.push({id,name,surname});
-    
     return id;
-    
-   }
+}
 
-   function removeEmployee(id) {
+export function removeEmployee(id) {
     let index = 0;
     for (let e of DATA.employees) {
      if (e.id===id) break;
      index++;
     }
     DATA.employees.splice(index, 1);
-   }
+}
 
-   
-   function showEmployee(employee) {
+function showEmployee(employee) {
     const keys = Object.keys(employee);
     console.log("Информация о сотруднике "+employee["name"]+":");
     for (let key of keys) {
@@ -43,61 +45,57 @@ function addEmployee(name, surname) {
     }
    }
 
-   function showEmployees() {
+function showEmployees() {
     // альтернативный вариант:
     // DATA.employees.forEach(showEmployee); 
     for (let e of DATA.employees) {
      showEmployee(e);
     }
-   }
+}
 
-   function findById(id) {
+export function findById(id) {
+    if (employeeMap[id]) {
+     return employeeMap[id];
+    }
     for (var e of DATA.employees) {
      if (id==e.id) {
+      employeeMap[id] = e;
       return e;
      }
     }
    }
-
    
-   const employeeMap = {};
-
-function findById(id) {
- if (employeeMap[id]) {
-  return employeeMap[id];
- }
- for (var e of DATA.employees) {
-  if (id==e.id) {
-   employeeMap[id] = e;
-   return e;
-  }
- }
-}
-
 function addPhone(id, phone) {
     const employee = findById(id);
+    if (!employee) {
+        return;
+    }
     const phones = employee.phones;
     if (!phones) {
      employee.phones = [];
     }
     employee.phones.push(phone);
-   }
-
-   
+}
 
 function setDateOfBirth(id, date) {
- const employee = findById(id);
- employee.dateOfBirth = date;
+    const employee = findById(id);
+    if (!employee) {
+        return;
+    }
+    employee.dateOfBirth = date;
 }
 
 function getAge(id) {
     const employee = findById(id);
+    if (!employee) {
+        return;
+    }
     let ageDiff = Date.now() - employee.dateOfBirth.getTime();
     let ageDate = new Date(ageDiff); // miliseconds from epoch
     return Math.abs(ageDate.getUTCFullYear() - 1970);
-   }
-
-   function formatDate(date) {
+}
+   
+function formatDate(date) {
     let day = date.getDate();
     if (day<10) day = '0'+day;
     let month = date.getMonth()+1;
@@ -107,9 +105,11 @@ function getAge(id) {
     return day + '.' + month + '.' + year;
    }
 
-   
-   function getEmployeeInfo(id) {
+function getEmployeeInfo(id) {
     const e = findById(id);
+    if (!e) {
+        return;
+    }
    
     const phones = e.phones?
      `Список телефонов: ${e.phones}`:'';
@@ -122,53 +122,37 @@ function getAge(id) {
      ${phones} 
      ${age}
     `;
-   }
+}
 
-   function testEmployee() {
-    addPhone(133, "555-55-55");
-    addPhone(133, "666-66-66");
-    setDateOfBirth(133, new Date(2000,1,1))
-    const info = getEmployeeInfo(133);
+function testEmployee() {
+    let id = addEmployee("Иван", "Иванов");
+    addPhone(id, "555-55-55");
+    addPhone(id, "666-66-66");
+    setDateOfBirth(id, new Date(2000,1,1))
+    const info = getEmployeeInfo(id);
     console.log(info);
-   }
+}
 
-   
-   function getEmployeeJSON(id) {
+function getEmployeeJSON(id) {
     const e = findById(id);
     return JSON.stringify(e);
-   }
+}
 
-   function removeEmployee(id) {
-    let index = 0;
-    for (let e of DATA.employees) {
-     if (e.id===id) break;
-     index++;
+export function setEmployeeManager(id, managerId) {
+    let employee = findById(id);
+    if (employee) {
+        employee.managerRef = managerId;
     }
-    DATA.employees.splice(index, 1);
-   }
- 
- 
+}
 
- function clearEmployeesPlaceholder() {
-    document.getElementById(PLACEHOLDER).innerHTML = '';
- }
- 
-
-
-
- function searchEmployees(name, surname, managerRef) {
+export function searchEmployees(name, surname, managerRef) {
     let results = [];
     for (let e of DATA.employees) {
-     if ((!name || e.name==name) &&
-      (!surname || e.surname==surname) &&
-      (!managerRef || e.managerRef==managerRef)) {
-      results.push(e);
-     }
+        if ((!name || e.name == name) &&
+            (!surname || e.surname == surname) &&
+            (!managerRef || e.managerRef == managerRef)) {
+            results.push(e);
+        }
     }
     return results;
-   }
-   
-
-   
-
-
+}
